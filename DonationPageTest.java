f8caab0f37d0ff1f2793c5f3ecd768b66fa7a0a5
Saif Akhter <130275596+sakhter313@users.noreply.github.com>
2025-10-23 23:10:52 +0530

    Update DonationPageTest.java

--- DonationPageTest.java
+++ DonationPageTest.java
@@ -1,22 +1,35 @@
 package tests;
 
 import base.BaseTest;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class DonationOptionsTest extends BaseTest {
+    private static final Logger logger = LogManager.getLogger(DonationOptionsTest.class);
+    
     @Test(priority = 3, description = "Ensure user can select both one-time and monthly donation options with different amounts")
     public void testDonationOptionsAndAmounts(){
-        //One-time donation
+        logger.info("Starting DonationOptionsTest: testDonationOptionsAndAmounts");
+        
+        // One-time donation
+        logger.info("Testing one-time donation with $50");
         donationPage.selectOneTimeDonation();
         donationPage.selectAmount("50");
-
-        //Reload for clean site
+        logger.debug("One-time donation selection completed");
+        
+        // Reload for clean state
         donationPage.openPage(configProps.getProperty("base.url"));
-
-        //Monthly donation
+        logger.debug("Page reloaded for monthly test");
+        
+        // Monthly donation
+        logger.info("Testing monthly donation with $100");
         donationPage.selectMonthlyDonation();
         donationPage.selectAmount("100");
+        logger.debug("Monthly donation selection completed");
+        
         Assert.assertTrue(true, "Donate Selection successful");
+        logger.info("DonationOptionsTest: testDonationOptionsAndAmounts completed successfully");
     }
 }
